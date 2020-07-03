0dec1a96adf8f799e2c550a98c0b7a59f3074c8e
tianwater <1012234190@qq.com>
2020-07-03 11:44:13 +0800

    购物车bag修改好

---
+++ js/common.js
@@ -0,0 +1,41 @@
+$(function () {
+    //nav
+    $(window).scroll(function () {
+        if ($("body,html").scrollTop() > 45) {
+            $(".fixed-nav").css({
+                "position": "fixed",
+                "top": "0",
+                "z-index": "999"
+            })
+        } else {
+            $(".fixed-nav").css({
+                "position": "relative"
+            })
+        }
+        if ($("body,html").scrollTop() > 45) {
+            $(".slip-sy").css({
+                "position": "fixed",
+                "top": "74px",
+                "z-index": "999"
+            })
+        }
+        else {
+            $(".slip-sy").css({
+                "position": "absolute",
+                "top": "119px"
+            })
+        }
+    })
+    $(".sy").mouseover(function () {
+        $(".slip-sy").css({ display: "block" })
+    })
+    $(".sy").mouseout(function () {
+        $(".slip-sy").css({ "display": "none" })
+    })
+    $(".slip-sy").mouseover(function () {
+        $(this).css({ "display": "block" })
+    })
+    $(".slip-sy").mouseout(function () {
+        $(this).css({ "display": "none" })
+    })
+})
